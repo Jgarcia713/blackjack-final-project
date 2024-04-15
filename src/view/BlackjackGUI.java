52378d31c2664785dae4843ee2d727b24071b97e
Jakob Garcia <jakobg@arizona.edu>
2024-04-15 15:09:36 -0700

    Started GUI background

--- src/view/BlackjackGUI.java
+++ src/view/BlackjackGUI.java
@@ -1,18 +1,29 @@
 package view;
 
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class BlackjackGUI extends Application {
-
+	
 	@Override
 	public void start(Stage stage) throws Exception {
-		BorderPane temp = new BorderPane();
+		BorderPane window = new BorderPane();
+
+		ImageView image = new ImageView(new Image(new FileInputStream("images/blackjack-background.png")));
+		image.setFitHeight(952);
+		image.setFitWidth(952);
+		image.setPreserveRatio(true);
+		window.setCenter(image);
 		
-		Scene scene = new Scene(temp, 800, 600);
+		Scene scene = new Scene(window, 950, 700);
 		stage.setScene(scene);
+		
 		stage.show();
 		
 	}
