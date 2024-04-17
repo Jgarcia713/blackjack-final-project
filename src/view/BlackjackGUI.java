8739bd078c3a85f4f3c89778faddeb9dc568d38c
siabrams <siabrams@arizona.edu>
2024-04-16 23:18:04 -0700

    Started work on CardSprite class

--- src/view/BlackjackGUI.java
+++ src/view/BlackjackGUI.java
@@ -1,25 +1,28 @@
 package view;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class BlackjackGUI extends Application {
+	Canvas canvas;
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		BorderPane window = new BorderPane();
-
+		canvas = new Canvas(800,800);
 		ImageView image = new ImageView(new Image(new FileInputStream("images/blackjack-background.png")));
 		image.setFitHeight(952);
 		image.setFitWidth(952);
 		image.setPreserveRatio(true);
-		window.setCenter(image);
+		StackPane window = new StackPane(image, canvas);
 		
 		Scene scene = new Scene(window, 950, 700);
 		stage.setScene(scene);
@@ -30,8 +33,13 @@ public class BlackjackGUI extends Application {
 		
 	}
 	
-	private void createSampleCard() {
-		
+	private void createSampleCard() throws FileNotFoundException {
+		CardSprite card = new CardSprite(100, 100, 0, null);
+		card.draw(canvas.getGraphicsContext2D());
+		CardSprite card1 = new CardSprite(300, 300, 0, null);
+		card1.draw(canvas.getGraphicsContext2D());
+		CardSprite card2 = new CardSprite(500, 500, 0, null);
+		card2.draw(canvas.getGraphicsContext2D());
 	}
 
 }
--- src/view/CardSprite.java
+++ src/view/CardSprite.java
@@ -1,18 +1,31 @@
 package view;
 
+import model.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public class CardSprite {
-
-    private GraphicsContext g;
     private Point2D position;
     private double rotation;
+    private Card card;
+    private Image img;
 
-    public CardSprite() {
-
+    public CardSprite(double x, double y, double rot, Card theCard) throws FileNotFoundException {
+    	position = new Point2D(x, y);
+    	rotation = rot;
+    	card = new Card(Rank.ACE, Suit.CLUBS); //theCard;
+    	
+    	if(card.getSuit() == Suit.CLUBS) {
+    		img = new Image(new FileInputStream("images/cCard.png"));
+    	}
     }
+    
     public void draw(GraphicsContext g) {
-
+    	g.drawImage(img, position.getX(), position.getY(), 122, 173);
     }
 }
