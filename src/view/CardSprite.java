465c1280bba2d95e385d8fe3a65cc7a63f7c5163
Jaysterr <j4ysterr@gmail.com>
2024-04-16 20:25:33 -0700

    added unimplemented cardsprite class (to be used for drawing a card)

---
+++ src/view/CardSprite.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
+public class CardSprite {
+
+    private GraphicsContext g;
+    private Point2D position;
+    private double rotation;
+
+    public CardSprite() {
+
+    }
+    public void draw(GraphicsContext g) {
+
+    }
+}
