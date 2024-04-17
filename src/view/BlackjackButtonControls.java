8321666b2fd5ebde9e27f74f022dcc936fa7ed0a
Jaysterr <j4ysterr@gmail.com>
2024-04-17 00:33:03 -0700

    GUI progress

---
+++ src/view/BlackjackButtonControls.java
@@ -0,0 +1,74 @@
+package view;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import model.Actions;
+import presenter.BlackjackGame;
+
+public class BlackjackButtonControls extends GridPane {
+
+    private BlackjackGame theGame;
+    private Button hit, stand, doubleDown, split;
+
+    private TextField betInput;
+    private Button placeBet;
+    public BlackjackButtonControls(BlackjackGame theModel) {
+        theGame = theModel;
+        initializePanel();
+        declareButtonEvents();
+    }
+
+    private void initializePanel() {
+        hit = new Button("Hit");
+        stand = new Button("Stand");
+        doubleDown = new Button("Double Down");
+        split = new Button("Split");
+
+        betInput = new TextField();
+        placeBet = new Button("Place bet");
+        this.add(hit, 0, 0);
+        this.add(stand, 1, 0);
+        this.add(doubleDown, 2, 0);
+        this.add(split, 3, 0);
+        this.add(betInput, 10, 0);
+        this.add(placeBet, 11, 0);
+        this.setPadding(new Insets(10));
+
+        this.setHgap(10);
+    }
+
+    private void declareButtonEvents() {
+        if (hit == null)
+            return;
+        hit.setOnAction(event -> {
+            theGame.makeMove(Actions.HIT);
+        });
+
+        if (stand == null)
+            return;
+        stand.setOnAction(event -> {
+            theGame.makeMove(Actions.STAND);
+        });
+
+        if (doubleDown == null)
+            return;
+        doubleDown.setOnAction(event -> {
+            theGame.makeMove(Actions.DOUBLE);
+        });
+
+        if (split == null)
+            return;
+        split.setOnAction(event -> {
+            theGame.makeMove(Actions.SPLIT);
+        });
+
+        if (placeBet == null)
+            return;
+        placeBet.setOnAction(event -> {
+
+        });
+    }
+
+}
--- src/view/BlackjackGUI.java
+++ src/view/BlackjackGUI.java
@@ -1,45 +1,100 @@
 package view;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import presenter.BlackjackGame;
+import javafx.animation.AnimationTimer;
 
 public class BlackjackGUI extends Application {
+
+	final int gameWidth = 1200;
+	final int gameHeight = 950;
 	Canvas canvas;
-	
+	GraphicsContext g;
+	Image background;
+
 	@Override
 	public void start(Stage stage) throws Exception {
-		canvas = new Canvas(800,800);
-		ImageView image = new ImageView(new Image(new FileInputStream("images/blackjack-background.png")));
-		image.setFitHeight(952);
-		image.setFitWidth(952);
-		image.setPreserveRatio(true);
-		StackPane window = new StackPane(image, canvas);
-		
-		Scene scene = new Scene(window, 950, 700);
+		// initialize objects
+		BlackjackGame game = new BlackjackGame();
+		BlackjackButtonControls buttons = new BlackjackButtonControls(game);
+		BorderPane pane = new BorderPane();
+
+
+
+		// create canvas
+		canvas = new Canvas(gameWidth,gameHeight-50);
+		g = canvas.getGraphicsContext2D();
+
+		// draw background (This should probably be moved to some render method so it's redrawn every frame
+		background = new Image(new FileInputStream("images/blackjack-background.png"));
+
+
+		// add nodes to pane
+		pane.setCenter(canvas);
+		pane.setBottom(buttons);
+
+		// initialize scene
+		Scene scene = new Scene(pane, gameWidth, gameHeight);
 		stage.setScene(scene);
 		
-		this.createSampleCard();
-		
+
+		final long startNanoTime = System.nanoTime();
+		// This is a timer that runs at 60 frames per second. It can be used for rendering the canvas
+		new AnimationTimer() {
+			public void handle(long currentNanoTime) {
+                try {
+                    render();
+					cardSpinAnimation(startNanoTime, currentNanoTime);
+				} catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+		}.start();
+
 		stage.show();
-		
 	}
 	
 	private void createSampleCard() throws FileNotFoundException {
 		CardSprite card = new CardSprite(100, 100, 0, null);
-		card.draw(canvas.getGraphicsContext2D());
+		card.draw(g);
 		CardSprite card1 = new CardSprite(300, 300, 0, null);
-		card1.draw(canvas.getGraphicsContext2D());
+		card1.draw(g);
 		CardSprite card2 = new CardSprite(500, 500, 0, null);
-		card2.draw(canvas.getGraphicsContext2D());
+		card2.draw(g);
 	}
 
+
+	private void render() throws FileNotFoundException {
+		//background
+		g.drawImage(background, 0, 0, canvas.getWidth(), canvas.getHeight());
+		//sample cards
+		createSampleCard();
+	}
+
+	private void cardSpinAnimation(long startNanoTime, long currentNanoTime) throws FileNotFoundException {
+		double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+		double x = 232 + 128 * Math.cos(t);
+		double y = 232 + 128 * Math.sin(t);
+
+		CardSprite card = new CardSprite(x+200, y, 0, null);
+		card.draw(g);
+		CardSprite card1 = new CardSprite(x, y-90, 0, null);
+		card1.draw(g);
+		CardSprite card2 = new CardSprite(x-100, y, 0, null);
+		card2.draw(g);
+
+	}
 }
